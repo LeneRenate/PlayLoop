6f3e777fa0b442abde8ba60f5ddb8fc113367ea8
LeneRenate <Lene.renate.rosseboe@gmail.com>
2026-01-27 20:46:26 +0100

    add comment for further adjustments and what to do next

--- src/LeneRenate/schemas/score.schema.js
+++ src/LeneRenate/schemas/score.schema.js
@@ -5,3 +5,10 @@ export const scoreSchema = z.object({
   nickname: z.string().min(3).max(10),
   score: z.number().int().positive().max(9999),
 });
+
+/***
+Look into/adjust:
+-score!
+-ban certain words/letter combos from nickname?
+-game_id for added games
+***/
